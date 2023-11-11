e4da219a97cec102c50665c6411ebf5549ce7d8f
NekhoroshkinaElena <muxaewaelena225@yandex.ru>
2023-11-11 12:14:12 +0300

    init commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,22 +1,30 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonSearch = findViewById<Button>(R.id.buttonSearch)
-        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                startActivity(Intent(this@MainActivity, SearchActivity::class.java))
-            }
+        val sharedPreferences = getSharedPreferences("Mode", Context.MODE_PRIVATE)
+        val darkMode = sharedPreferences.getBoolean("night", false)
+
+        if (darkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
+
+        val buttonSearch = findViewById<Button>(R.id.buttonSearch)
+        val buttonClickListener: View.OnClickListener = View.OnClickListener {
+            startActivity(Intent(this@MainActivity, SearchActivity::class.java))
+        }
+
         buttonSearch.setOnClickListener(buttonClickListener)
 
         val buttonLibrary = findViewById<Button>(R.id.buttonLibrary)
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,17 +1,76 @@
 package com.example.playlistmaker
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        val switch = findViewById<SwitchCompat>(R.id.darkTheme)
+
+        val sharedPreferences = getSharedPreferences("Mode", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        val nightMode = sharedPreferences.getBoolean("night", false)
+
+        if (nightMode) {
+            switch.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("night", true)
+                editor.apply()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("night", false)
+                editor.apply()
+            }
+        }
+
         val buttonBack = findViewById<ImageButton>(R.id.buttonBack)
         buttonBack.setOnClickListener {
             finish()
+        }
+
+        val shareButton = findViewById<Button>(R.id.share)
+        shareButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain" +
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.android_developer))
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share)))
+        }
+
+
+        val supportButton = findViewById<Button>(R.id.support)
+        supportButton.setOnClickListener {
+            val message = getString(R.string.message_for_developer)
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.your_email)))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            shareIntent.putExtra(
+                Intent.EXTRA_SUBJECT, getString(R.string.subject_message)
+            )
+            startActivity(shareIntent)
+        }
+
+        val agreementButton = findViewById<Button>(R.id.user_agreement)
+        agreementButton.setOnClickListener {
+            val agreementIntent = Intent(Intent.ACTION_VIEW)
+            agreementIntent.data = Uri.parse(getString(R.string.practicum_offer))
+            startActivity(agreementIntent)
         }
     }
 }
