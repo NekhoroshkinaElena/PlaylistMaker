6639276674ca234925a4e3630a9a742aeb6699a4
NekhoroshkinaElena <muxaewaelena225@yandex.ru>
2024-02-02 19:04:03 +0300

    feat: Добавить debounce для обработки нажатия на элемент списка результатов поиска, чтобы минимизировать риски открытия нескольких аудиоплееров одновременно feat: Автоматизировать выполнение запроса при помощи debounce feat: Реализовать логику воспроизведения отрывка трека на экране «Аудиоплеер»

--- app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
@@ -16,7 +19,23 @@ const val IMAGE_CORNER_RADIUS = 8
 
 class AudioPlayerActivity : AppCompatActivity() {
 
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+        private const val TIME_UPDATE_DELAY = 500L
+    }
+
+    private var playerState = STATE_DEFAULT
+
+    private var mediaPlayer = MediaPlayer()
+
     private lateinit var binding: ActivityAudioPlayerBinding
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    private val run = timerUpdater()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,5 +73,77 @@ class AudioPlayerActivity : AppCompatActivity() {
             binding.collectionName.isVisible = true
             binding.collectionNameValue.isVisible = true
         }
+
+        preparePlayer(track?.previewUrl)
+
+        binding.playTrack.setOnClickListener {
+            playbackControl()
+        }
+    }
+
+    private fun preparePlayer(previewUrl: String?) {
+        mediaPlayer.setDataSource(previewUrl)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            handler.removeCallbacks(run)
+            playerState = STATE_PREPARED
+            binding.playTrack.setImageResource(R.drawable.ic_play)
+            mediaPlayer.seekTo(0)
+            binding.trackTime.text =
+                millisecondToMinute(mediaPlayer.currentPosition.toString())
+        }
+    }
+
+    private fun playbackControl() {
+        when (playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+
+            STATE_PAUSED, STATE_PREPARED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        binding.playTrack.setImageResource(R.drawable.ic_button_pause)
+        playerState = STATE_PLAYING
+        handler.post(run)
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        binding.playTrack.setImageResource(R.drawable.ic_play)
+        playerState = STATE_PAUSED
+        handler.removeCallbacks(run)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        handler.removeCallbacks(run)
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    private fun timerUpdater(): Runnable {
+        return object : Runnable {
+            override fun run() {
+                if (playerState == STATE_PLAYING) {
+                    binding.trackTime.text =
+                        millisecondToMinute(mediaPlayer.currentPosition.toString())
+                    handler.postDelayed(this, TIME_UPDATE_DELAY)
+                }
+            }
+        }
     }
 }
+
